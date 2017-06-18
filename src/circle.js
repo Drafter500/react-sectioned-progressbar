e39d83bb7d8cf52779984e0d543dce72eb5f23e9
Drafter500 <vzemtsov@yahoo.com>
2017-06-18 22:11:02 +0300

    add draft of the component

---
+++ src/circle.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { getSectionAngle, calculateSectionsCoords } from './utils'
+
+
+class Circle extends React.Component {
+
+  calculateRadius() {
+    return 90;
+  }
+
+  render() {
+    const {
+      sectionsNumber,
+      thickness,
+    } = this.props;
+
+    const angleDeg = getSectionAngle(sectionsNumber);  
+    const radius = this.calculateRadius();   
+    const sectionLength = Math.PI*radius*angleDeg/180;
+
+    const style = {
+      fill: 'none', 
+      stroke: 'green', 
+      strokeDasharray: `${sectionLength} ${sectionLength}`,
+      'strokeWidth': 16,
+      'strokeDashoffset': '5px',
+    };
+
+    const centerX = thickness/2 + radius;
+    const centerY = thickness/2 + radius;
+
+    const outputSize = 2*radius + thickness;
+
+    const sectionCoords = calculateSectionsCoords(sectionsNumber, radius, thickness);
+
+    return (    
+      <svg width={`${outputSize}`} height={`${outputSize}`}>  
+        <g transform={`rotate(50 ${centerX} ${centerY})`}>
+        {sectionCoords.map(section => {
+            const { x0, y0, x, y } = section;
+            return <path d={`M ${x0}, ${y0} A90,90 0 0 1 ${x}, ${y}`} style={style}/>
+          })
+        }        
+        </g>
+      </svg>
+    );
+  }
+}
+
+Circle.proptypes = {
+  sectionsNumber: PropTypes.number,
+  thickness: PropTypes.number,
+}
+
+Circle.defaultProps = {
+  sectionsNumber: 10,
+  thickness: 15,
+}
+
+export default Circle;
---
+++ src/utils.js
@@ -0,0 +1,36 @@
+const RADIAN = 180 / Math.PI;
+
+export function getSectionAngle(sectionsNumber) {
+    return 360 / sectionsNumber;
+}
+
+export function calculateSectionsCoords(sectionsNumber, radius, thickness) {
+    const sectionCoords = [];
+
+    const startX = radius + thickness/2;
+    let startY = thickness/2;
+
+    let prevX = startX;
+    let prevY = startY;
+
+    // Shift coordinates - we need to start at the top of the circle, not in the center
+    const angleShift = (90 / RADIAN); 
+    startY += radius;
+
+    const angleRad = getSectionAngle(sectionsNumber) / RADIAN;
+
+    for (let i = 0; i < sectionsNumber; ++i) {
+        const newX = startX + radius * Math.cos(angleRad * (i + 1) - angleShift);
+        const newY = startY + radius * Math.sin(angleRad * (i + 1) - angleShift);
+        sectionCoords.push({
+        x0: prevX,
+        y0: prevY,
+        x: newX, 
+        y: newY, 
+        });  
+        prevX = newX;
+        prevY = newY;      
+    }
+
+    return sectionCoords;
+}
