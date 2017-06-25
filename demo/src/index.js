17e5657014e94b446773a728d510311902c27c8b
Drafter500 <vzemtsov@yahoo.com>
2017-06-25 21:57:25 +0300

    Add proress

--- demo/src/index.js
+++ demo/src/index.js
@@ -5,10 +5,12 @@ import SectionedProgressbar from '../../src/SectionedProgressbar';
 
 ReactDOM.render(
   <SectionedProgressbar
-    sizePx={500}
-    thickness={35}
-    sectionsNumber={7}
-    sectionsGapPercent={10}
+    sizePx={300}
+    thickness={25}
+    total={7}
+    progress={2}
+    sectionsGapPercent={30}
+    className="custom"
   />,
   document.getElementById('root'),
 );
--- src/SectionedProgressbar.js
+++ src/SectionedProgressbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import { getSectionAngle, calculateSectionsCoords } from './utils';
 
 
@@ -11,8 +12,8 @@ class SectionedProgressbar extends React.Component {
   }
 
   calculateRotationAngle() {
-    const { sectionsNumber, sectionsGapPercent } = this.props;
-    return 360 / sectionsNumber / 100 * sectionsGapPercent / 2;
+    const { total, sectionsGapPercent } = this.props;
+    return 360 / total / 100 * sectionsGapPercent / 2;
   }
 
   calculateGapInPixels(sectionLength) {
@@ -22,13 +23,14 @@ class SectionedProgressbar extends React.Component {
 
   render() {
     const {
-      sectionsNumber,
+      total,
       thickness,
       className,
+      progress,
     } = this.props;
 
     const radius = this.calculateRadius();
-    const angleDeg = getSectionAngle(sectionsNumber);
+    const angleDeg = getSectionAngle(total);
     const sectionLength = Math.PI * radius * (angleDeg / 180);
 
     const gapPx = this.calculateGapInPixels(sectionLength);
@@ -45,7 +47,7 @@ class SectionedProgressbar extends React.Component {
 
     const outputSize = 2 * radius + thickness;
 
-    const sectionCoords = calculateSectionsCoords(sectionsNumber, radius, thickness);
+    const sectionCoords = calculateSectionsCoords(total, radius, thickness);
     const rotationAngle = this.calculateRotationAngle();
 
     return (
@@ -55,12 +57,13 @@ class SectionedProgressbar extends React.Component {
         height={`${outputSize}`}
       >
         <g transform={`rotate(${rotationAngle} ${centerX} ${centerY})`}>
-          {sectionCoords.map((section) => {
+          {sectionCoords.map((section, i) => {
             const { x0, y0, x, y } = section;
+            const sectionDone = i < progress;
             return (
               <path
                 d={`M ${x0}, ${y0} A${radius},${radius} 0 0 1 ${x}, ${y}`}
-                className={'sectionedProgressBar-section'}
+                className={classnames('sectionedProgressBar-section', { active: sectionDone })}
                 style={style}
               />
             );
@@ -74,7 +77,8 @@ class SectionedProgressbar extends React.Component {
 
 SectionedProgressbar.propTypes = {
   sizePx: PropTypes.number,
-  sectionsNumber: PropTypes.number,
+  total: PropTypes.number,
+  progress: PropTypes.number,
   thickness: PropTypes.number,
   sectionsGapPercent: PropTypes.number,
   className: PropTypes.string,
@@ -82,7 +86,8 @@ SectionedProgressbar.propTypes = {
 
 SectionedProgressbar.defaultProps = {
   sizePx: 200,
-  sectionsNumber: 10,
+  total: 10,
+  progress: 0,
   thickness: 15,
   sectionsGapPercent: 5,
   className: '',
